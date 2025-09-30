839457461b98ed3e5c2b2581f8f9df742db4d811
yeounnae <168003012+yeounnae@users.noreply.github.com>
2025-09-30 10:44:58 +0900

    0930 강의실습

---
+++ Ch06/src/sec01/exam01/Calcul.java
@@ -0,0 +1,12 @@
+package sec01.exam01;
+
+public class Calcul {
+	int add(int a, int b)
+	{
+		return a+b;
+	}
+	double add(double a, double b)
+	{
+		return a+b;
+	}
+}
---
+++ Ch06/src/sec01/exam01/MainClass.java
@@ -0,0 +1,19 @@
+package sec01.exam01;
+
+public class MainClass {
+	public static void main(String[] args) {
+		Student st1 = new Student();
+//		st1.SayHello();
+//		Car c = new Car("Hyundai", 200);
+//		c.SetMaxSpeed(100);
+//		c.maxSpeed = 200;
+//		c.ShowInfo();
+		
+		Calcul cal = new Calcul();
+		int val = cal.add(1, 3);
+		double vald = cal.add(1.0, 2.0);
+		System.out.println("Value "+ val);
+
+	}
+
+}
