d09c2b02ecf33c55760feb701b0521ca2de862e6
yeounnae <168003012+yeounnae@users.noreply.github.com>
2025-10-02 14:46:31 +0900

    1002 수업실습

---
+++ Ch06/src/sec03/exam01/MainClass.java
@@ -0,0 +1,19 @@
+package sec03.exam01;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car c1 = new Car();
+		Car c2 = new Car("Hyundai");
+		Car c3 = new Car("Kia", "Sedan");
+		Car c4 = new Car("Kia", "truck", 150);
+		
+		c1.ShowInfo();
+		c2.ShowInfo();
+		c3.ShowInfo();
+		c4.ShowInfo();
+		System.out.println("");
+	}
+
+}
