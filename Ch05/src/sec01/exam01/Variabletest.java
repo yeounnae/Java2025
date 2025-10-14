deebf7af8da47ef55a80310b0e6ded35e10fed1b
sieun <168003012+yeounnae@users.noreply.github.com>
2025-10-14 20:00:28 +0900

    0923 ~ 0925 수업실습
    파일이 다 지워져서 다시 올립니다

---
+++ Ch05/src/sec01/exam01/Variabletest.java
@@ -0,0 +1,17 @@
+package sec01.exam01;
+
+public class Variabletest {
+		public static void main(String[] args) {
+			int a = 0;
+			int b = a;
+			
+			String str1 = null;
+			String str2 = str1;
+			
+			System.out.println("Hi.");
+			
+			int[] intArray = null;
+			intArray[0] = 10;
+		}
+
+	}
