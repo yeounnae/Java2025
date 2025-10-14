051847cc165d51659a72f62200ced37b9e46d0d6
sieun <168003012+yeounnae@users.noreply.github.com>
2025-10-14 21:04:11 +0900

    Chap 06 책 예제

---
+++ Chap06/src/sec03/exam01/CarExample.java
@@ -0,0 +1,10 @@
+package sec03.exam01;
+
+public class CarExample {
+	public static void main(String[] args) {
+		Car myCar = new Car("검정", 3000);
+		//Car myCar = new Car(); < 기본 생성자 호출 불가
+
+	}
+
+}
