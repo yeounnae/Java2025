e05d908a9f2cf0af4ae175eb9a8d922de5858502
sieun <168003012+yeounnae@users.noreply.github.com>
2025-11-20 21:36:11 +0900

    Ch07-1 책 예제

---
+++ Ch07sec01/src/sec01/exam05/VeryImportantPerson.java
@@ -0,0 +1,5 @@
+package sec01.exam05;
+
+//Member 상속 불가능
+public class VeryImportantPerson extends Member {
+}
---
+++ Ch07sec01/src/sec01/exam07/pack2/D.java
@@ -0,0 +1,10 @@
+package sec01.exam07.pack2;
+import sec01.exam07.pack1.A;
+
+public class D extends A {
+	public D() {
+	 super();
+	 this.field = "value";
+	 this.method();
+	}
+}
